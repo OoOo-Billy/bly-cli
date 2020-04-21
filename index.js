f61e35be4b1816d4071042fb6d3ff5bf7743b77e
Billy <leiyu_bi@foxmail.com>
2020-04-21 23:47:41 +0800

    CommandRunner Done!

--- index.js
+++ index.js
@@ -4,29 +4,47 @@
 
 const CONFIG = require("./config");
 
-console.log("config", CONFIG);
+const commands = process.argv.slice(2);
 
-// const commands = process.argv.slice(2);
+const runner = new CommandRunner(registerCommands(CONFIG));
 
-// console.log("commands", commands);
+start();
 
-// function registerCommands(commands) {
-
-// }
-
-function runCommand(command) {
-  // TODO
+function start() {
+  if(!commands.length) return;
+  runner.run(commands);
 }
 
-const METHODS = {
-  getVersion() {
+function registerCommands(config) {
+  const mapCommand = {};
+  for(let key in config) {
+    if(key.includes("COMMANDS")) {
+      const pre = key.split("_")[0];
+      config[key].forEach(i => {
+        const c = i.split(":");
+        mapCommand[config[pre + "_TEMPLATE"].replace("$", c[0])] = c[1];
+      });
+    }
+  }
+
+  return mapCommand;
+}
+
+function CommandRunner(map) {
+
+  this.run = function(commandName) {
+
+    this[map[commandName[0]]](commandName.slice(1));
+  }
+
+  this.getVersion = function() {
     console.log("v" + CONFIG.VERSION);
     return CONFIG.VERSION;
   }
+  this.getHelp = function(c) {
+    console.log("no help", c);
+  }
+  this.runScript = function(c) {
+    console.log("running...", c);
+  }
 }
-
-
-// function registerComFnGenerator(type) {
-//   function registerCommandsFn() {}
-//   return function() {}
-// }
