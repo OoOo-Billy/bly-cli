8e2cd578112ca524b45abfc93b2693979f23b55c
Billy <1403809057@qq.com>
2020-07-30 18:37:54 +0800

    ✅ Add router generator.

--- lib/generators/templates/vue/src/App.vue
+++ lib/generators/templates/vue/src/App.vue
@@ -1,13 +1,26 @@
+<%_ if (renderOptions.router) { _%>
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+<%_ } else { _%>
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js <% if (renderOptions.plugins.typescript) { _%>+ TypeScript <% } _%>App" />
   </div>
 </template>
+<%_ } _%>
 
-<%_ if (renderOptions.plugins.typescript) { _%>
+<%_ if (!renderOptions.router) { _%>
+  <%_ if (renderOptions.plugins.typescript) { _%>
 <script lang="ts">
-<%_ if (!renderOptions.classComponent) { _%>
+    <%_ if (!renderOptions.classComponent) { _%>
 import Vue from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
 
@@ -17,7 +30,7 @@ export default Vue.extend({
     HelloWorld
   }
 });
-<%_ } else { _%>
+    <%_ } else { _%>
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from './components/HelloWorld.vue';
 
@@ -27,9 +40,9 @@ import HelloWorld from './components/HelloWorld.vue';
   },
 })
 export default class App extends Vue {}
-<%_ } _%>
+    <%_ } _%>
 </script>
-<%_ } else { _%>
+  <%_ } else { _%>
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
@@ -40,6 +53,7 @@ export default {
   }
 }
 </script>
+  <%_ } _%>
 <%_ } _%>
 
 <%_ if (renderOptions.cssPreprocessor !== 'stylus') { _%>
@@ -58,8 +72,39 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  <%_ if (renderOptions.router) { _%>
+}
+    <%_ if (!renderOptions.cssPreprocessor) { _%>
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+    <%_ } else { _%>
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+    <%_ } _%>
+  <%_ } else { _%>
   margin-top: 60px;
 }
+  <%_ } _%>
 </style>
 <%_ } else { _%>
 <style lang="stylus">
@@ -69,6 +114,16 @@ export default {
   -moz-osx-font-smoothing grayscale
   text-align center
   color #2c3e50
+  <%_ if (renderOptions.router) { _%>
+  #nav
+    padding 30px
+    a
+      font-weight bold
+      color #2c3e50
+      &.router-link-exact-active
+        color #42b983
+  <%_ } else { _%>
   margin-top 60px
+  <%_ } _%>
 </style>
 <%_ } _%>
